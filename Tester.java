fde3ee01663d358c129f5d1129a5bc88f0bcd646
dib1000 <dib5017@gmail.com>
2020-10-31 20:45:55 -0400

    I tested the get method and it works

--- Tester.java
+++ Tester.java
@@ -4,8 +4,17 @@ public class Tester {
   System.out.println(a);
   System.out.println(a.size());
   for(int i = 0; i<=10; i++) {
-    a.add("hi");
-    System.out.println(a.size());
-  }
+    if(i%2==0) {
+      a.add("hi");
+      System.out.println(a.size());
+    }
+    else {
+      a.add("bye");
+      System.out.println(a.size());
+    }
+    }
+    for(int i = 0; i<10; i++) {
+      System.out.println(a.get(i));
+    }
   }
 }
