08762f511124c8e9b89153bf2c4d19186d2eb2dc
dib1000 <dib5017@gmail.com>
2020-11-07 20:05:43 -0500

    I added the removeDuplicates method

--- Demo.java
+++ Demo.java
@@ -12,14 +12,34 @@ public class Demo{
       }
     }
   }
+  public static SuperArray findOverlap(SuperArray a, SuperArray b) {
+    SuperArray overlap = new SuperArray();
+    for(int i = 0; i<a.size(); i++) {
+      for(int j = 0; j<b.size(); j++) {
+        if(a.get(i).equals(b.get(j))) {
+          overlap.add(a.get(i));
+        }
+      }
+    }
+    removeDuplicates(overlap);
+    return overlap;
+  }
   public static void main(String[]args){
     SuperArray words = new SuperArray();
     //grouped to save vertical space
-    words.add("kani");   words.add("kani");    words.add("kani");    words.add("uni");     words.add("ebi");     words.add("una");
+    words.add("kani");   words.add("kani");    words.add("kani");    words.add("uni");     words.add("una");
     words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
     words.add("una");    words.add("ebi");     words.add("toro");    words.add("toro");    words.add("toro");
 
     System.out.println(words);
     removeDuplicates(words);
-    System.out.println(words);  }
+    System.out.println(words);
+
+    System.out.println();
+    SuperArray nums = new SuperArray();
+    SuperArray numz = new SuperArray();
+    nums.add("9"); nums.add("1"); nums.add("2"); nums.add("2"); nums.add("3"); nums.add("4");
+    numz.add("0"); numz.add("4"); numz.add("2"); numz.add("2"); numz.add("9");
+    System.out.println(findOverlap(nums,numz));
+  }
 }
