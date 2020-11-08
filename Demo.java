b1c40a6e468f2838c77edeb3f70cca0509403648
dib1000 <dib5017@gmail.com>
2020-11-07 19:51:27 -0500

    I added the removeDuplicates method

--- Demo.java
+++ Demo.java
@@ -1,11 +1,13 @@
 public class Demo{
   public static void removeDuplicates(SuperArray s){
     for(int i = 0; i<s.size(); i++) {
-      for(int j = i+1; j<s.size(); j++ ) {
-        String comparison = s.get(i);
-        String compare = s.get(j);
-        if(compare.equals(comparison)) {
+      int j = i+1;
+      while(j<s.size()) {
+        if(s.get(j).equals(s.get(i))) {
           s.remove(j);
+        }
+        else {
+          j+=1;
         }
       }
     }
@@ -13,9 +15,9 @@ public class Demo{
   public static void main(String[]args){
     SuperArray words = new SuperArray();
     //grouped to save vertical space
-    words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
+    words.add("kani");   words.add("kani");    words.add("kani");    words.add("uni");     words.add("ebi");     words.add("una");
     words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
-    words.add("una");    words.add("ebi");     words.add("toro");
+    words.add("una");    words.add("ebi");     words.add("toro");    words.add("toro");    words.add("toro");
 
     System.out.println(words);
     removeDuplicates(words);
